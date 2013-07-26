cb48dbe9eac8a739a743604236c71377569a36f8
henry4j <henry4js@gmail.com>
2013-07-26 11:01:16 -0700

    .

--- sources/text/src/test/java/com/henry4j/AppTest.java
+++ sources/text/src/test/java/com/henry4j/AppTest.java
@@ -45,88 +45,87 @@ public class AppTest {
      curl -o /tmp/tfidf-vectors -kL https://dl.dropboxusercontent.com/u/47820156/mahout/tfidf-vectors
  */
 
-    @Test
-    public void testOfJakeMannixIdeaAndQuestions() { // jake.mannix@gmail.com 
-        val conf = new Configuration();
-        val dictionary = readDictionary(new Path("/tmp/dictionary.file-0"), conf);
-        assertThat(dictionary.length, equalTo(41807));
+@Test
+public void testDocumentTopicInferenceForNewDocsOverReuters() {
+    val conf = new Configuration();
+    val dictionary = readDictionary(new Path("/tmp/dictionary"), conf);
+    assertThat(dictionary.length, equalTo(41807));
 
-        // tfidf_vector represents a document in RandomAccessSparseVector.
-        val tfidf_vector = readTFVectorsInRange(new Path("/tmp/tfidf-vectors"), conf, 0, 1)[0].getSecond();
-        assertThat(tfidf_vector.size(), equalTo(41807));
+    // reads 'model' dense matrix (20 x 41K), and in 'topicSum' dense vector.
+    TopicModel model = readModel(dictionary, new Path("/tmp/lda-model-splits"), conf);
+    assertThat(model.getNumTopics(), equalTo(20));
+    assertThat(model.getNumTerms(), equalTo(41807));
 
-        // reads 'model' dense matrix (20 x 41K), and in 'topicSum' dense vector.
-        TopicModel model = readModel(dictionary, new Path("/tmp/reuters-lda-model-splits"), conf);
-        assertThat(model.getNumTopics(), equalTo(20));
-        assertThat(model.getNumTerms(), equalTo(41807));
+    val doc = takeOnlineDocument(conf);
+    Vector docTopics = new DenseVector(new double[model.getNumTopics()]).assign(1.0/model.getNumTopics());
+    Matrix docTopicModel = new SparseRowMatrix(model.getNumTopics(), doc.size());
 
-        val doc = tfidf_vector;
-        Vector docTopics = new DenseVector(new double[model.getNumTopics()]).assign(1.0/model.getNumTopics());
-        Matrix docTopicModel = new SparseRowMatrix(model.getNumTopics(), doc.size());
-
-        // Q: How many iterations are good? Why?
-        for (int i = 0; i < 25 /* maxItrs */; i++) {
-            model.trainDocTopicModel(doc, docTopics, docTopicModel);
-            System.out.println(docTopics.toString());
-            // Q: Do you think that 'model' got mutated, or not? why?
-        }
-
-        Vector inferred = model.infer(doc, docTopics);
-        System.out.println(inferred); // Q: What is this inferred? How can I use it?
+    for (int i = 0; i < 25 /* maxItrs */; i++) {
+        model.trainDocTopicModel(doc, docTopics, docTopicModel);
+        System.out.println(docTopics.toString());
     }
+}
 
-    @SneakyThrows({ IOException.class })
-    private static Pair<String, Vector>[] readTFVectorsInRange(Path path, Configuration conf, int offset, int length) {
-        val seq = new SequenceFile.Reader(FileSystem.get(conf), path, conf);
-        val documentName = new Text();
-        @SuppressWarnings("unchecked")
-        Pair<String, Vector>[] vectors = new Pair[length];
-        VectorWritable vector = new VectorWritable();
-        for (int i = 0; i < offset + length && seq.next(documentName, vector); i++) {
-            if (i >= offset) {
-                vectors[i - offset] = Pair.of(documentName.toString(), vector.get());
-            }
-        }
-        return vectors;
-    }
+private static Vector takeOnlineDocument(Configuration conf) {
+    // tfidf_vector represents a document in RandomAccessSparseVector.
+    // TODO: make a TFIDF vector out of an online string agaist dictionary, and frequency files.
+    val tfidf_vector = readTFVectorsInRange(new Path("/tmp/tfidf-vectors"), conf, 0, 1)[0].getSecond();
+    assertThat(tfidf_vector.size(), equalTo(41807));
+    return tfidf_vector;
+}
 
-    @SneakyThrows({ IOException.class })
-    private static TopicModel readModel(String[] dictionary, Path path, Configuration conf) {
-        double alpha = 0.0001; // default: doc-topic smoothing
-        double eta = 0.0001; // default: term-topic smoothing
-        double modelWeight = 1f;
-        return new TopicModel(conf, eta, alpha, dictionary, 1, modelWeight, listModelPath(path, conf));
-    }
-
-    @SneakyThrows({ IOException.class })
-    private static Path[] listModelPath(Path path, Configuration conf) {
-        if (FileSystem.get(conf).isFile(path)) {
-            return new Path[] { path };
-        } else {
-            val statuses = FileSystem.get(conf).listStatus(path, PathFilters.partFilter());
-            val modelPaths = new Path[statuses.length];
-            for (int i = 0; i < statuses.length; i++) {
-                modelPaths[i] = new Path(statuses[i].getPath().toUri().toString());
-            }
-            return modelPaths;
+@SneakyThrows({ IOException.class })
+private static Pair<String, Vector>[] readTFVectorsInRange(Path path, Configuration conf, int offset, int length) {
+    val seq = new SequenceFile.Reader(FileSystem.get(conf), path, conf);
+    val documentName = new Text();
+    @SuppressWarnings("unchecked")
+    Pair<String, Vector>[] vectors = new Pair[length];
+    VectorWritable vector = new VectorWritable();
+    for (int i = 0; i < offset + length && seq.next(documentName, vector); i++) {
+        if (i >= offset) {
+            vectors[i - offset] = Pair.of(documentName.toString(), vector.get());
         }
     }
+    return vectors;
+}
 
-    @SneakyThrows({ IOException.class })
-    private static String[] readDictionary(Path path, Configuration conf) {
-        val term = new Text();
-        val id = new IntWritable();
-        val reader = new SequenceFile.Reader(FileSystem.get(conf), path, conf);
-        val termIds = ImmutableList.<Pair<String, Integer>>builder();
-        int maxId = 0;
-        while (reader.next(term, id)) {
-            termIds.add(Pair.of(term.toString(), id.get()));
-            maxId = max(maxId, id.get());
+@SneakyThrows({ IOException.class })
+private static TopicModel readModel(String[] dictionary, Path path, Configuration conf) {
+    double alpha = 0.0001; // default: doc-topic smoothing
+    double eta = 0.0001; // default: term-topic smoothing
+    double modelWeight = 1f;
+    return new TopicModel(conf, eta, alpha, dictionary, 1, modelWeight, listModelPath(path, conf));
+}
+
+@SneakyThrows({ IOException.class })
+private static Path[] listModelPath(Path path, Configuration conf) {
+    if (FileSystem.get(conf).isFile(path)) {
+        return new Path[] { path };
+    } else {
+        val statuses = FileSystem.get(conf).listStatus(path, PathFilters.partFilter());
+        val modelPaths = new Path[statuses.length];
+        for (int i = 0; i < statuses.length; i++) {
+            modelPaths[i] = new Path(statuses[i].getPath().toUri().toString());
         }
-        String[] terms = new String[maxId + 1];
-        for (val termId : termIds.build()) {
-            terms[termId.getSecond().intValue()] = termId.getFirst().toString();
-        }
-        return terms;
+        return modelPaths;
     }
+}
+
+@SneakyThrows({ IOException.class })
+private static String[] readDictionary(Path path, Configuration conf) {
+    val term = new Text();
+    val id = new IntWritable();
+    val reader = new SequenceFile.Reader(FileSystem.get(conf), path, conf);
+    val termIds = ImmutableList.<Pair<String, Integer>>builder();
+    int maxId = 0;
+    while (reader.next(term, id)) {
+        termIds.add(Pair.of(term.toString(), id.get()));
+        maxId = max(maxId, id.get());
+    }
+    String[] terms = new String[maxId + 1];
+    for (val termId : termIds.build()) {
+        terms[termId.getSecond().intValue()] = termId.getFirst().toString();
+    }
+    return terms;
+}
 }
