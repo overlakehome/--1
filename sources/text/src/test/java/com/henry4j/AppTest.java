c8af02deef77e03542476792f604e5a3250b2d1c
henry4j <henry4js@gmail.com>
2013-07-26 00:16:02 -0700

    .

--- sources/text/src/test/java/com/henry4j/AppTest.java
+++ sources/text/src/test/java/com/henry4j/AppTest.java
@@ -18,12 +18,15 @@ import org.apache.hadoop.io.Text;
 import org.apache.mahout.clustering.lda.cvb.TopicModel;
 import org.apache.mahout.common.Pair;
 import org.apache.mahout.common.iterator.sequencefile.PathFilters;
+import org.apache.mahout.math.DenseVector;
+import org.apache.mahout.math.Matrix;
+import org.apache.mahout.math.SparseRowMatrix;
 import org.apache.mahout.math.Vector;
 import org.apache.mahout.math.VectorWritable;
+import org.apache.mahout.utils.vectors.VectorHelper;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableList;
-import com.henry4j.commons.base.Actions.Action2;
 
 public class AppTest {
     @Test
@@ -31,6 +34,10 @@ public class AppTest {
         // hadoop dfs -get /tmp/mahout-work-hylee/reuters-out-seqdir-sparse-lda/dictionary.file-0 /tmp/dictionary.file-0
         // hadoop dfs -get /tmp/mahout-work-hylee/reuters-lda-model/model-20 /tmp/model-n
         // hadoop dfs -getmerge /tmp/mahout-work-hylee/reuters-out-seqdir-sparse-lda/tfidf-vectors /tmp/tfidf-vectors
+
+        // https://dl.dropboxusercontent.com/u/47820156/mahout/dictionary.file-0
+        // https://dl.dropboxusercontent.com/u/47820156/mahout/tfidf-vectors
+        // https://dl.dropboxusercontent.com/u/47820156/mahout/reuters-lda-model
 
         val conf = new Configuration();
         val dictionary = readDictionary(new Path("/tmp/dictionary.file-0"), conf);
@@ -41,20 +48,24 @@ public class AppTest {
         assertThat(tfidf_vector.size(), equalTo(41807));
 
         // reads 'model' dense matrix (20 x 41K), and in 'topicSum' dense vector.
-        TopicModel model = readModel(dictionary, new Path("/tmp/model-n"), conf);
+        TopicModel model = readModel(dictionary, new Path("/tmp/reuters-lda-model-splits"), conf);
         assertThat(model.getNumTopics(), equalTo(20));
         assertThat(model.getNumTerms(), equalTo(41807));
 
-        
-        // model.trainDocTopicModel(original, topics, docTopicModel);
-        // /tmp/tfidf
-        // model.infer(original, docTopics);
+        val doc = tfidf_vector;
+        Vector docTopics = new DenseVector(new double[model.getNumTopics()]).assign(1.0/model.getNumTopics());
+        Matrix docTopicModel = new SparseRowMatrix(model.getNumTopics(), doc.size());
+        for (int i = 0; i < 20 /* maxItrs */; i++) {
+            model.trainDocTopicModel(doc, docTopics, docTopicModel);
+        }
 
-//        Vector doc = null;
-//        Vector docTopics = new DenseVector(numTopics).assign(1.0/numTopics);
-//        Matrix docModel = new SparseRowMatrix(numTopics, doc.get().size());
-//    
-//        SequenceFileReader<, V>
+        val s = docTopics.toString();
+        assertThat(s, equalTo("{0:6.046094820493859E-4,1:0.05851704029725188,2:0.003614000476232148," 
+                + "3:0.001996149462372238,4:0.18642287604760513,5:0.06476163674725903,6:5.777280589039363E-4,"
+                + "7:0.0020414226678579944,8:0.017255405563081586,9:0.1989598480718128,10:0.00915192390925309,"
+                + "11:2.5002725299438455E-4,12:0.0013006654940801582,13:0.018706280748127756,14:2.575510963867921E-4,"
+                + "15:0.002869875877356748,16:0.03140740540525602,17:0.008377486326233984,18:0.10405585384287644,"
+                + "19:0.28887221317300843}"));
     }
 
     // http://mail-archives.apache.org/mod_mbox/mahout-user/201205.mbox/%3CCAKA-QbDg4DR3RTbv8KoYnMOnLXkbbqaXji0+WtrL5UdSdsKbOA@mail.gmail.com%3E
@@ -87,12 +98,16 @@ public class AppTest {
 
     @SneakyThrows({ IOException.class })
     private static Path[] listModelPath(Path path, Configuration conf) {
-        val statuses = FileSystem.get(conf).listStatus(path, PathFilters.partFilter());
-        val modelPaths = new Path[statuses.length];
-        for (int i = 0; i < statuses.length; i++) {
-            modelPaths[i] = new Path(statuses[i].getPath().toUri().toString());
+        if (FileSystem.get(conf).isFile(path)) {
+            return new Path[] { path };
+        } else {
+            val statuses = FileSystem.get(conf).listStatus(path, PathFilters.partFilter());
+            val modelPaths = new Path[statuses.length];
+            for (int i = 0; i < statuses.length; i++) {
+                modelPaths[i] = new Path(statuses[i].getPath().toUri().toString());
+            }
+            return modelPaths;
         }
-        return modelPaths;
     }
 
     @SneakyThrows({ IOException.class })
