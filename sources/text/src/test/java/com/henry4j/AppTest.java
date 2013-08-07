ea5ccc76f486974f4c3b248ceab334b73b9ee39f
henry4j <henry4js@gmail.com>
2013-08-07 01:30:57 -0700

    .

--- sources/text/src/test/java/com/henry4j/AppTest.java
+++ sources/text/src/test/java/com/henry4j/AppTest.java
@@ -1,6 +1,8 @@
 package com.henry4j;
 
+import static java.lang.Math.abs;
 import static java.lang.Math.max;
+import static java.lang.Math.round;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.Assert.assertThat;
@@ -14,8 +16,10 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
+import org.apache.lucene.search.similarities.DefaultSimilarity;
 import org.apache.mahout.clustering.lda.cvb.TopicModel;
 import org.apache.mahout.common.Pair;
 import org.apache.mahout.common.iterator.sequencefile.PathFilters;
@@ -42,21 +46,32 @@ hadoop dfs -get /tmp/mahout-work-$USER/reuters-out-seqdir-sparse-lda/tfidf-vecto
 hadoop dfs -get /tmp/mahout-work-$USER/reuters-lda-model/model-20 /tmp/lda-model-splits
 
 # or,
-curl -o /tmp/dictionary -kL https://dl.dropboxusercontent.com/u/47820156/mahout/dictionary
+curl -o /tmp/dictionary.file-0 -kL https://dl.dropboxusercontent.com/u/47820156/mahout/unigram/dictionary.file-0
 curl -o /tmp/lda-model-splits/part-r-0000\#1 -kL https://dl.dropboxusercontent.com/u/47820156/mahout/lda-model-splits/part-r-0000[0-9] --create-dir
 curl -o /tmp/tfidf-vectors -kL https://dl.dropboxusercontent.com/u/47820156/mahout/tfidf-vectors
  */
+    static DefaultSimilarity similarity = new DefaultSimilarity();
+
+    @Test
+    public void testIDF() {
+        val conf = new Configuration();
+        val idf = readIdfs(new Path("/tmp/df-count"), conf);
+        assertThat(idf.length, equalTo(394));
+
+        
+        similarity.tf(10);
+    }
 
     @Test
     public void testDocumentTopicInferenceForNewDocsOverReuters() {
         val conf = new Configuration();
-        val dictionary = readDictionary(new Path("/tmp/dictionary"), conf);
-        assertThat(dictionary.length, equalTo(41807));
+        val dictionary = readDictionary(new Path("/tmp/dictionary.file-0"), conf);
+        assertThat(dictionary.length, equalTo(394));
 
         // reads 'model' dense matrix (20 x 41K), and in 'topicSum' dense vector.
-        TopicModel model = readModel(dictionary, new Path("/tmp/lda-model-splits"), conf);
+        TopicModel model = readModel(dictionary, new Path("/tmp/model-splits"), conf);
         assertThat(model.getNumTopics(), equalTo(20));
-        assertThat(model.getNumTerms(), equalTo(41807));
+        assertThat(model.getNumTerms(), equalTo(394));
 
         val doc = takeOnlineDocument(conf);
         Vector docTopics = new DenseVector(new double[model.getNumTopics()]).assign(1.0 / model.getNumTopics());
@@ -64,14 +79,15 @@ curl -o /tmp/tfidf-vectors -kL https://dl.dropboxusercontent.com/u/47820156/maho
 
         for (int i = 0; i < 20 /* maxItrs */; i++) {
             model.trainDocTopicModel(doc, docTopics, docTopicModel);
-            System.out.println(strip(docTopics, 0.1).toString());
+            System.out.println(strip(docTopics, 0.02).toString());
         }
 
         // tests against doc-topic inference from training
-        assertThat(Math.round(docTopics.get(0) - 31), lessThanOrEqualTo(1L));
-        assertThat(Math.round(docTopics.get(3) - 22), lessThanOrEqualTo(1L));
-        assertThat(Math.round(docTopics.get(7) - 22), lessThanOrEqualTo(1L));
-        assertThat(Math.round(docTopics.get(10) - 13), lessThanOrEqualTo(1L));
+        assertThat(abs(round(100 * docTopics.get(17) - 29)), lessThanOrEqualTo(0L));
+        assertThat(abs(round(100 * docTopics.get(16) - 21)), lessThanOrEqualTo(0L));
+        assertThat(abs(round(100 * docTopics.get(19) - 20)), lessThanOrEqualTo(0L));
+        assertThat(abs(round(100 * docTopics.get(8) - 17)), lessThanOrEqualTo(0L));
+        assertThat(abs(round(100 * docTopics.get(4) - 11)), lessThanOrEqualTo(0L));
     }
 
     private static Vector strip(Vector v, double min) {
@@ -88,7 +104,7 @@ curl -o /tmp/tfidf-vectors -kL https://dl.dropboxusercontent.com/u/47820156/maho
         // tfidf_vector represents a document in RandomAccessSparseVector.
         // TODO: make a TFIDF vector out of an online string agaist dictionary, and frequency files.
         val tfidf_vector = readTFVectorsInRange(new Path("/tmp/tfidf-vectors"), conf, 0, 1)[0].getSecond();
-        assertThat(tfidf_vector.size(), equalTo(41807));
+        assertThat(tfidf_vector.size(), equalTo(394));
         return tfidf_vector;
     }
 
@@ -135,7 +151,7 @@ curl -o /tmp/tfidf-vectors -kL https://dl.dropboxusercontent.com/u/47820156/maho
         val id = new IntWritable();
         val reader = new SequenceFile.Reader(FileSystem.get(conf), path, conf);
         val termIds = ImmutableList.<Pair<String, Integer>> builder();
-        int maxId = 0;
+        int maxId = -1;
         while (reader.next(term, id)) {
             termIds.add(Pair.of(term.toString(), id.get()));
             maxId = max(maxId, id.get());
@@ -145,5 +161,28 @@ curl -o /tmp/tfidf-vectors -kL https://dl.dropboxusercontent.com/u/47820156/maho
             terms[termId.getSecond().intValue()] = termId.getFirst().toString();
         }
         return terms;
+    }
+
+    @SneakyThrows({ IOException.class })
+    private static double[] readIdfs(Path path, Configuration conf) {
+        val id = new IntWritable();
+        val frequency = new LongWritable();
+        val reader = new SequenceFile.Reader(FileSystem.get(conf), path, conf);
+        val idFrequencies = ImmutableList.<Pair<Integer, Long>> builder();
+        int maxId = -1;
+        long numDocs = 0;
+        while (reader.next(id, frequency)) {
+            if (-1 == id.get()) {
+                numDocs = frequency.get();
+            } else {
+                idFrequencies.add(Pair.of(id.get(), frequency.get()));
+                maxId = max(maxId, id.get());
+            }
+        }
+        double[] idfs = new double[maxId + 1];
+        for (val idFrequency : idFrequencies.build()) {
+            idfs[idFrequency.getFirst().intValue()] = similarity.idf(idFrequency.getSecond().longValue(), numDocs);
+        }
+        return idfs;
     }
 }
